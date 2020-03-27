0f450852d06d7e2244a788f1801aae53deccda9b
gon <g.casmi@gmail.com>
2020-03-27 19:03:27 +0100

    initial commit

---
+++ src/com/exception/RevisionDiariaException.java
@@ -0,0 +1,13 @@
+package com.exception;
+
+public class RevisionDiariaException extends Exception {
+	  /**
+	 * 
+	 */
+	private static final long serialVersionUID = 6228365948450089254L;
+
+	public RevisionDiariaException(String mensaje) {
+	    super(mensaje);
+	  }
+	}
+
